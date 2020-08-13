d28dee73046660c217505bb3e50a2affd08796d6
Folayan Adesola <48876487+folayarn@users.noreply.github.com>
2020-08-13 18:49:39 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:FetchApi')
-                ->dailyAt("00:30");
+                ->daily();
     }
 
     /**
